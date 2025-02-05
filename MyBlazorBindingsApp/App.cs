9fc8674d943e9f5ae317e99f86c50c86b47b5781
LiberRuth <124418235+LiberRuth@users.noreply.github.com>
2025-02-05 17:32:25 +0900

    초기 커밋.

---
+++ MyBlazorBindingsApp/App.cs
@@ -0,0 +1,11 @@
+﻿using BlazorBindings.Maui;
+
+namespace MyBlazorBindingsApp
+{
+    public class App : BlazorBindingsApplication<AppShell>
+    {
+        public App(IServiceProvider services) : base(services)
+        {
+        }
+    }
+}
